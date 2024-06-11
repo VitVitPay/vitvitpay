844858c5574104828c26a8eaff31b412f5c82215
michaela budde <michaelabudde87@gmail.com>
2024-06-10 23:04:14 -0700

    nav design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,56 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Blank from '../components/Blank'
-import { Welcome } from '../welcome/welcome'
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet, View } from "react-native";
+import { navBarStyle } from "./index.jsx";
+import Blank from "../components/Blank";
+import { Welcome } from "../welcome/welcome";
 
 const Tab = createBottomTabNavigator();
 
-function HomeNavBar({}){
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Welcome}/>
-            <Tab.Screen name="Wallet" component={Blank}/>
-            <Tab.Screen name="Send" component={Blank}/>
-            <Tab.Screen name="Balance" component={Blank}/>
-            <Tab.Screen name="Profile" component={Blank}/>
-        </Tab.Navigator>
-    )
+function HomeNavBar({}) {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 20,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: "#fff",
+          borderRadius: 15,
+          height: 70,
+          ...styles.shadow,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Welcome}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            <View>
+              <Image source={require("../../assets/vvp.home.inactive.png")} />
+              <Text></Text>
+            </View>;
+          },
+        }}
+      />
+      <Tab.Screen name="Wallet" component={Blank} />
+      <Tab.Screen name="Send" component={Blank} />
+      <Tab.Screen name="Balance" component={Blank} />
+      <Tab.Screen name="Profile" component={Blank} />
+    </Tab.Navigator>
+  );
 }
-
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "#7D83F4",
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
 module.exports = {
-    HomeNavBar,
-}
+  HomeNavBar,
+};
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,36 +1,47 @@
 import { StyleSheet } from "react-native";
 
 const formButtonStyle = StyleSheet.create({
-    darkPurpleButton: {
-        backgroundColor: "#4F46CC",
-        padding: 16,
-        borderRadius: 30,
-        marginVertical: 10,
-        width: "40%",
-    },
-    fadedPurpleButton: {
-        backgroundColor: "#7D83F4",
-        padding: 16,
-        borderRadius: 30,
-        marginVertical: 10,
-        width: "40%",
-    },
-    darkPurpleButtonText: {
-        fontSize: 25,
-        fontStyle: "normal",
-        fontWeight: "600",
-        color: "#E7E8FE",
-        textAlign: "center",
-    },
-    fadedPurpleButtonText: {
-        fontSize: 25,
-        fontStyle: "normal",
-        fontWeight: "600",
-        color: "#E7E8FE",
-        textAlign: "center",
-    },
-})
+  darkPurpleButton: {
+    backgroundColor: "#4F46CC",
+    padding: 16,
+    borderRadius: 30,
+    marginVertical: 10,
+    width: "40%",
+  },
+  fadedPurpleButton: {
+    backgroundColor: "#7D83F4",
+    padding: 16,
+    borderRadius: 30,
+    marginVertical: 10,
+    width: "40%",
+  },
+  darkPurpleButtonText: {
+    fontSize: 25,
+    fontStyle: "normal",
+    fontWeight: "600",
+    color: "#E7E8FE",
+    textAlign: "center",
+  },
+  fadedPurpleButtonText: {
+    fontSize: 25,
+    fontStyle: "normal",
+    fontWeight: "600",
+    color: "#E7E8FE",
+    textAlign: "center",
+  },
+});
+const navBarStyle = StyleSheet.create({
+  navBarIcon: {
+    width: 25,
+    height: 25,
+  },
+  navBarText: {
+    // color: focused ? "#7D83F4" : "#B0B2FF",
+    fontSize: 20,
+  },
+});
 
 module.exports = {
-    formButtonStyle,
-}
+  formButtonStyle,
+  navBarStyle,
+};

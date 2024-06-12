05f9458475c25bce273d341baf5937384f790c86
michaela budde <michaelabudde87@gmail.com>
2024-06-11 19:29:27 -0700

    nav bar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ function HomeNavBar({}) {
             case "Send":
               iconName = focused
                 ? require("../../assets/vitvitpay_logo.1.png")
-                : require("../../assets/vitvitpay_logo.1.png");
+                : require("../../assets/vvp_send.inactive.png");
               iconStyle = styles.navBarIcon;
               break;
             case "Balance":
@@ -61,9 +61,10 @@ function HomeNavBar({}) {
             </View>
           );
         },
+        tabBarLabel: () => null, // Hide default tab labels
         tabBarStyle: {
           position: "absolute",
-          bottom: 20,
+          bottom: 10,
           left: 20,
           right: 20,
           elevation: 0,
@@ -103,118 +104,3 @@ const styles = StyleSheet.create({
 module.exports = {
   HomeNavBar,
 };
-
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import { StyleSheet, View } from "react-native";
-// import { navBarStyle } from "./index.jsx";
-// import Blank from "../components/Blank";
-// import { Welcome } from "../welcome/welcome";
-
-// const Tab = createBottomTabNavigator();
-
-// function HomeNavBar({}) {
-//   return (
-//     <Tab.Navigator
-//       screenOptions={{
-//         tabBarStyle: {
-//           position: "absolute",
-//           bottom: 20,
-//           left: 20,
-//           right: 20,
-//           elevation: 0,
-//           backgroundColor: "#fff",
-//           borderRadius: 15,
-//           height: 70,
-//           ...styles.shadow,
-//         },
-//       }}
-//     >
-//       <Tab.Screen
-//         name="Home"
-//         component={Welcome}
-//         options={{
-//           tabBarIcon: ({ focused }) => {
-//             <View>
-//               <Image source={require("../../assets/vvp.home.inactive.png")} />
-//               <Text>Home</Text>
-//             </View>;
-//           },
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Wallet"
-//         component={Blank}
-//         options={{
-//           tabBarIcon: ({ focused }) => {
-//             <View>
-//               <Image source={require("../../assets/vvp.wallet.inactive.png")} />
-//               <Text>Wallet</Text>
-//             </View>;
-//           },
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Send"
-//         component={Blank}
-//         options={{
-//           tabBarIcon: ({ focused }) => {
-//             <View>
-//               <Image source={require("../../assets/vitvitpay_logo.1.png")} />
-//               <Text>Send</Text>
-//             </View>;
-//           },
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Balance"
-//         component={Blank}
-//         options={{
-//           tabBarIcon: ({ focused }) => {
-//             <View>
-//               <Image
-//                 source={require("../../assets/vvp.balance.inactive.png")}
-//               />
-//               <Text>Balance</Text>
-//             </View>;
-//           },
-//         }}
-//       />
-//       <Tab.Screen
-//         name="Profile"
-//         component={Blank}
-//         options={{
-//           tabBarIcon: ({ focused }) => {
-//             <View>
-//               <Image
-//                 source={require("../../assets/vvp.profile.inactive.png")}
-//               />
-//               <Text>Profile</Text>
-//             </View>;
-//           },
-//         }}
-//       />
-//     </Tab.Navigator>
-//   );
-// }
-// const styles = StyleSheet.create({
-//   shadow: {
-//     shadowColor: "#7D83F4",
-//     shadowOffset: { width: 0, height: 10 },
-//     shadowOpacity: 0.25,
-//     shadowRadius: 4,
-//     elevation: 5,
-//   },
-//   navBarIcon: {
-//     width: 25,
-//     height: 25,
-//     tintColor: focused ? "#7D83F4" : "#B0B2FF",
-//   },
-//   navBarText: {
-//     color: focused ? "#7D83F4" : "#B0B2FF",
-//     fontSize: 20,
-//   },
-// });
-
-// module.exports = {
-//   HomeNavBar,
-// };
